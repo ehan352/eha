d5bcbc707f0ce3b9c51c4c8cf63f088d627b9dd2
ehan352 <muhammadraihanalmutawaqin@gmail.com>
2024-08-28 08:03:18 +0700

    Add files via upload

---
+++ ulangtahun.js
@@ -0,0 +1,2 @@
+// Script to dynamically change the birthday message (if needed)
+document.getElementById('birthday-message').textContent = "Hope your special day brings you all that your heart desires!";
